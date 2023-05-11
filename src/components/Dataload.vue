70a168c7b720a66a43864129352bcaa81335c767
marcelo magalhaes <magawebtec@gmail.com>
2023-05-11 14:45:04 -0300

    teste fech vue test

---
+++ src/components/Dataload.vue
@@ -0,0 +1,22 @@
+<!--
+Say Hello World with Vue!
+-->
+
+<script setup>
+import { ref } from 'vue'
+
+// A "ref" is a reactive data source that stores a value.
+// Technically, we don't need to wrap the string with ref()
+// in order to display it, but we will see in the next
+// example why it is needed if we ever intend to change
+// the value.
+const message = ref('Hello World!')
+const URL = import.meta.env.PUBLIC_URL
+const response = await fetch(URL + '/api/clhdtusbo0000lbyc6y8yntkc/note/clhjf3u850000mn08ofd4sk7w/show');
+const data = await response.json();
+console.log(data);
+</script>
+
+<template>
+  <h1>{{ data }}</h1>
+</template>
