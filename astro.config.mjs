cdefbe43457f626a101df0e543302f1a18c6176c
marcelo magalhaes <magawebtec@gmail.com>
2023-05-12 20:57:03 -0300

    ssr+

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 import react from "@astrojs/react";
-
 import vue from "@astrojs/vue";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
