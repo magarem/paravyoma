3590f6c8b402502cba59069a0029909b30f06355
Marcelo Magalhaes <contato@magaweb.com.br>
2025-03-12 14:30:46 -0300

    new updates - layout footer copyright year

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
     <html lang="en">
 		<Head>
-			<Title>My App</Title>
+			<Title>Paravyoma Yogaterapia</Title>
 			<!-- <Meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" /> -->
 		</Head>
         <Header title="testeheader" />
@@ -10,7 +10,7 @@
             <slot />
             <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
                 <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-                    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://magaweb.com.br/" class="hover:underline">Magaweb</a>. All Rights Reserved.
+                    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="https://magaweb.com.br/" class="hover:underline">Magaweb</a>. All Rights Reserved.
                     </span>
                 </div>
             </footer>
